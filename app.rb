55e159eea2d05471a799914a9d2d034ca3e0f16a
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-03 23:37:32 +0100

    new climb button added to climbs page.

--- app.rb
+++ app.rb
@@ -4,3 +4,7 @@ require("sinatra/contrib/all")
 require_relative("./controllers/climbers_controller")
 require_relative("./controllers/climbs_controller")
 require_relative("./controllers/routes_controller")
+
+get '/' do
+  erb(:index)
+end
